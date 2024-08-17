b58fe257d5beb375d483f97ef8141d0cc66238ee
Paul Fioravanti <paul.fioravanti@gmail.com>
2024-08-17 16:26:52 +1000

    Fix minor type issue

--- plover_steno_engine_hooks_logger/tool.py
+++ plover_steno_engine_hooks_logger/tool.py
@@ -8,7 +8,7 @@ from plover.gui_qt.tool import Tool
 from .hooks import StenoEngineHooksLogger
 
 # REF: https://stackoverflow.com/a/28727066/567863
-class StenoEngineHooksMetaClass(type(StenoEngineHooksLogger), type(Tool)): # type: ignore
+class StenoEngineHooksMetaClass(type(StenoEngineHooksLogger), type(Tool)):
     """
     Metaclass to prevent the following error:
     `TypeError: metaclass conflict: the metaclass of a derived class must be a
@@ -16,7 +16,7 @@ class StenoEngineHooksMetaClass(type(StenoEngineHooksLogger), type(Tool)): # typ
     """
 
 class StenoEngineHooksLoggerGUITool(
-    Tool, # type: ignore
+    Tool,
     StenoEngineHooksLogger,
     metaclass=StenoEngineHooksMetaClass
 ):
